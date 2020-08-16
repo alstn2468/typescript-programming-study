ea38c6091958937d6c073faca8c7903bf4ced1d7
alst2468 <alstn2468_@naver.com>
2020-08-16 19:07:59 +0900

    [#133] Add zip util function for merge object keys and values

---
+++ Summary/ms/Chapter_12/Code/ch12_1/src/utils/index.ts
@@ -0,0 +1,8 @@
+import {
+    getFileNameAndNumber,
+    FileNameAndNumber,
+} from "./getFileNameAndNumber";
+import { range } from "./range";
+import { zip } from "./zip";
+
+export { getFileNameAndNumber, FileNameAndNumber, range, zip };
---
+++ Summary/ms/Chapter_12/Code/ch12_1/src/utils/zip.ts
@@ -0,0 +1,12 @@
+export const zip = (keys: string[], values: any[]) => {
+    const makeObject = (key: string, value: any) => ({ [key]: value });
+    const mergeObject = (a: any[]) =>
+        a.reduce((sum, val) => ({ ...sum, ...val }), {});
+
+    let tmp = keys
+        .map((key, index) => [key, values[index]])
+        .filter((a) => a[0] && a[1])
+        .map((a) => makeObject(a[0], a[1]));
+
+    return mergeObject(tmp);
+};
