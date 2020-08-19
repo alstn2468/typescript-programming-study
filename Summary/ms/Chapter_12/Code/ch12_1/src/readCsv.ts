d71f8c8e6d0f280a25c1c4c64c7bd1aa43cff66a
Kimminsu <alstn2468_@naver.com>
2020-08-19 09:29:32 +0900

    [#135] Add test image at window env

--- Summary/ms/Chapter_12/Code/ch12_1/src/readCsv.ts
+++ Summary/ms/Chapter_12/Code/ch12_1/src/readCsv.ts
@@ -1,7 +1,7 @@
 import { getFileNameAndNumber } from "./utils";
 import { csvFileReaderGenerator } from "./csv/csvFileReaderGenerator";
 
-const [filename] = getFileNameAndNumber("./data/fake-1.csv", 1);
+const [filename] = getFileNameAndNumber("./data/fake-100000.csv", 1);
 
 let line = 1;
 
