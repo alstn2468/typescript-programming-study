dee431ae7247756c9fb09323b18d67cf2dadfbe7
alst2468 <alstn2468_@naver.com>
2020-08-21 21:08:34 +0900

    [#139] Add insert document at collection example

--- Summary/ms/Chapter_12/Code/ch12_2/src/test/insertDocumnet-Test.ts
+++ Summary/ms/Chapter_12/Code/ch12_2/src/test/insertDocumnet-Test.ts
@@ -1,6 +1,6 @@
 import { connect } from "../mongodb/connect";
 
-const makeCollectionsTest = async () => {
+const insertDocumentTest = async () => {
     let connection;
 
     try {
@@ -8,7 +8,14 @@ const makeCollectionsTest = async () => {
         const db = await connection.db("ch12-2");
         const personsCollection = db.collection("persons");
         const addressesCollection = db.collection("addresses");
-        console.log(personsCollection, addressesCollection);
+
+        const person = { name: "Jack", age: 32 };
+        let result = await personsCollection.insertOne(person);
+        console.log(result);
+
+        const address = { country: "korea", city: "seoul" };
+        result = await addressesCollection.insertOne(address);
+        console.log(result);
     } catch (e) {
         console.log(e.message);
     } finally {
@@ -16,4 +23,4 @@ const makeCollectionsTest = async () => {
     }
 };
 
-makeCollectionsTest();
+insertDocumentTest();
