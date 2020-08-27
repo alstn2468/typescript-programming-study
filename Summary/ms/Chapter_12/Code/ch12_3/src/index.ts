91825fbd41ee425329273d6bac3bda1ba3746faa
alst2468 <alstn2468_@naver.com>
2020-08-27 20:02:06 +0900

    [#150] Add url routing example and summary

--- Summary/ms/Chapter_12/Code/ch12_3/src/index.ts
+++ Summary/ms/Chapter_12/Code/ch12_3/src/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express(),
     port = 4000;
 
-app.get("/", (req, res) => res.json({ message: "Hello World!" })).listen(
-    port,
-    () => console.log(`http://localhost:${port} started...`)
-);
+app.get("/", (req, res) => res.json({ message: "Hello World!" }))
+    .get("/users/:skip/:limit", (req, res) => {
+        const { skip, limit } = req.params;
+        res.json({ skip, limit });
+    })
+    .listen(port, () => console.log(`http://localhost:${port} started...`));
