29fee70e96b8f9070bd67da95ea72d0afbf0bb0c
alst2468 <alstn2468_@naver.com>
2020-09-26 21:59:20 +0900

    [#159] Add useEffect summary

--- Summary/ms/Chapter_12/Code/users/src/App.tsx
+++ Summary/ms/Chapter_12/Code/users/src/App.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IUser } from "./IUser";
 import { getDataPromise } from "./getDataPromise";
 
 const App: React.FC = () => {
     const [users, setUsers] = useState<IUser[]>([]);
-    getDataPromise((receivedUsers: IUser[]) =>
-        setUsers([...users, ...receivedUsers])
-    )(0, 1);
+
+    useEffect(() => {
+        getDataPromise((receivedUsers: IUser[]) =>
+            setUsers([...users, ...receivedUsers])
+        )(0, 1);
+    }, [])
 
     return <div className="App">{JSON.stringify(users)}</div>;
 };
